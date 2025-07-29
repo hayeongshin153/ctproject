d0459a1ef4044247bb4c204808f762bd90639f74
hayeongshin153 <sinhy713@naver.com>
2025-07-29 12:13:29 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,28 +1,30 @@
 import streamlit as st
 import streamlit.components.v1 as components
-st.set_page_config(layout='wide', page_title='Time table!!!')
 
+st.set_page_config(layout='wide', page_title='컴퓨팅사고력 문제 by SHY')
 st.title('This is SHY Webapp!!')
 
-with open('./new version(origin) copy 2.html', 'r', encoding='utf-8') as f:
-    html = f.read()
+col1, col2 = st.columns((4, 1))
 
-col1, col2 = st.columns((4,1))
 with col1:
     with st.expander('Content #1...'):
         url = 'https://www.youtube.com/watch?v=XyEOEBsa8I4'
         st.info('Content..')
         st.video(url)
 
-    with st.expander('Content #2...'):
-        components.html(html, height=800, scrolling=True)  
+    with st.expander('오늘은 어디로 가야할까? 수업 장소를 찾아라!'):
+        with open('./new version(origin) copy 2.html', 'r', encoding='utf-8') as f:
+            html = f.read()
+        components.html(html, height=800, scrolling=True)
 
     with st.expander('Content #3...'):
-        components.html(html, height=800, scrolling=True)  
+        with open('./index copy 3.html', 'r', encoding='utf-8') as f:
+            html = f.read()
+        components.html(html, height=800, scrolling=True)
 
 with col2:
     with st.expander('Tips..'):
         st.info('Tips..')
 
 st.markdown('<hr>', unsafe_allow_html=True)
-st.write('<font color="BLUE">(c)copyright. all rights reserved by SHY', unsafe_allow_html=True)
+st.write('<font color="BLUE">(c)copyright. all rights reserved by SHY</font>', unsafe_allow_html=True)
