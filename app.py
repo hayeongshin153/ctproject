d81390b063b398c8fbdea77e18d41fe30623adb8
hayeongshin153 <sinhy713@naver.com>
2025-07-31 00:53:32 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -9,18 +9,18 @@ st.title('This is SHY Webapp!!')
 col1, col2 = st.columns((3, 1))  # 예: 왼쪽이 75%, 오른쪽이 25%
 
 with col1:
-    with st.expander('누구에게 문서를 전해줘야할까?'):
+    with st.expander('1.누구에게 문서를 전해줘야할까?'):
         st.image("image.png", caption="문제 이미지", use_container_width=True)
         with open('./new version(origin) copy.html', 'r', encoding='utf-8') as f:
             html = f.read()
         components.html(html, height=800, scrolling=True)
 
-    with st.expander('오늘은 어디로 가야할까? 수업 장소를 찾아라!'):
+    with st.expander('2.오늘은 어디로 가야할까? 수업 장소를 찾아라!'):
         with open('./new version(origin) copy 3.html', 'r', encoding='utf-8') as f:
             html = f.read()
         components.html(html, height=800, scrolling=True)
 
-    with st.expander('오늘은 어디로 가야할까? 수업 장소를 찾아라!(AI.ver)...'):
+    with st.expander('3.오늘은 어디로 가야할까? 수업 장소를 찾아라!(applied AI.ver)'):
         with open('./new version(origin) copy 2.html', 'r', encoding='utf-8') as f:
             html = f.read()
         components.html(html, height=800, scrolling=True)
